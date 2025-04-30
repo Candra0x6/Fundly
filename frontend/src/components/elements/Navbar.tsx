07ea12166f1ba06742c652145714f4b03faf9d66
Candra0x6 <kurniawancandram@gmail.com>
2025-04-30 17:49:02 +0700

    refactor: update Navbar component to integrate logo, simplify navigation, and enhance authentication handling

--- frontend/src/components/elements/Navbar.tsx
+++ frontend/src/components/elements/Navbar.tsx
@@ -1,46 +1,22 @@
-import React, { useState, useEffect } from 'react'
 import { Button } from '../ui/button'
+import logo from '@/assets/logo.svg'
 import { useAuth } from '@/utility/use-auth-client'
-
+import { useNavigate } from 'react-router-dom'
 function Navbar() {
-  const [isScrolled, setIsScrolled] = useState(false)
-  const { login } = useAuth()
+  // TODO wallet identity
+  const navigate = useNavigate()
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true)
-      } else {
-        setIsScrolled(false)
-      }
-    }
-
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+  const { principal, user, isAuthenticated } = useAuth()
 
   return (
-    <header className={`border-b border-zinc-100 w-full z-50 bg-white ${isScrolled ? 'fixed' : 'relative'}`}>
+    <header className={`border-b border-zinc-100 w-full z-50 bg-white fixed`}>
       <div className="container mx-auto py-4 px-4 flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <div className="h-10 w-10 rounded-xl bg-emerald-500 flex items-center justify-center">
-            <span className="text-white font-bold">FND</span>
-          </div>
-          <span className="font-semibold text-xl">Fundify</span>
+        <div className="flex items-center gap-2 cursor-pointer" onClick={() => navigate('/')}>
+          <img src={logo} alt="Fundly" className="h-10 w-10 rounded-xl" />
+          <span className="font-semibold text-xl text-emerald-800">Fundly</span>
         </div>
-        <nav className="hidden md:flex items-center gap-8 ">
-          <a
-            href="#how-it-works"
-            className="text-sm font-medium text-zinc-600 hover:text-emerald-600 transition-colors"
-          >
-            How It Works
-          </a>
-          <a href="#msmes" className="text-sm font-medium text-zinc-600 hover:text-emerald-600 transition-colors">
-            For MSMEs
-          </a>
-          <a href="#investors" className="text-sm font-medium text-zinc-600 hover:text-emerald-600 transition-colors">
-            For Investors
-          </a>
+        <nav className=" items-center gap-8 ">
+
           <a
             href="/marketplace"
             className="text-sm font-medium text-zinc-600 hover:text-emerald-600 transition-colors"
@@ -48,12 +24,12 @@ function Navbar() {
             Marketplace
           </a>
         </nav>
-        <div className="flex items-center gap-3 ">
-          <Button variant="ghost" className="text-sm">
-            Log In
-          </Button>
-          <Button className="bg-emerald-500 hover:bg-emerald-600 text-sm" onClick={login}>Register</Button>
-        </div>
+        {isAuthenticated ? <div className="flex items-center gap-3 ">
+
+          <Button className="bg-emerald-500 hover:bg-emerald-600 text-sm rounded-[0.5rem]" onClick={() => navigate('/auth')}>Login</Button>
+        </div> : <div className="flex items-center gap-3 ">
+          <Button className="bg-emerald-500 hover:bg-emerald-600 text-sm rounded-[0.5rem]" onClick={() => navigate('/auth')}>Login</Button>
+        </div>}
       </div>
     </header>
   )
