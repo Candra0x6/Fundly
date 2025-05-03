364166f264592671a0dad28d8550ffba9d60953b
Candra0x6 <kurniawancandram@gmail.com>
2025-05-03 10:08:37 +0700

    feat: update PropertyGridSkeleton to use NFTCardSkeleton for improved loading representation in property grid

--- frontend/src/components/skeleton/PropertyGridSkeleton.tsx
+++ frontend/src/components/skeleton/PropertyGridSkeleton.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import PropertyCardSkeleton from './PropertyCardSkeleton'
-import { cn } from '@/lib/utils'
 
-function PropertyGridSkeleton({length = 8, className}:{length?:number, className?:string}) {
+import { cn } from '@/lib/utils'
+import NFTCardSkeleton from './PropertyCardSkeleton'
+
+function PropertyGridSkeleton({ length = 8, className }: { length?: number, className?: string }) {
   return (
     <div className={cn("w-full h-full grid items-start grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4", className)}>
-      {Array.from({length}).map((_) => 
-        <PropertyCardSkeleton/>
+      {Array.from({ length }).map((_) =>
+        <NFTCardSkeleton />
       )}
     </div>
   )
